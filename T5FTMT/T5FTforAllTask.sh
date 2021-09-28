795af5e3a462f3871558396ff5aca5558f045ad5
qcw <qcwthu@126.com>
2021-09-28 19:24:55 +0800

    34234342

---
+++ T5FTMT/T5FTforAllTask.sh
@@ -0,0 +1,37 @@
+learnrate=(5e-5)
+allstartindex=(0 1 2)
+for onerate in ${learnrate[@]}
+do
+  for onestartindex in ${allstartindex[@]}
+  do
+              echo "------------------------------"
+              python -m torch.distributed.launch --nproc_per_node 1 --master_port 29977 T5FTforAllTask.py \
+                --cuda 4 \
+                --lr $onerate \
+                --startindex $onestartindex \
+                --weight_decay 0.01 \
+                --max_grad_norm 1.0 \
+                --batch_size_per_gpu 1 \
+                --valid_size_per_gpu 8 \
+                --test_size_per_gpu 8 \
+                --gradient_accumulation_steps 4 \
+                --max_epoch 256 \
+                --num_workers 0 \
+                --save_step 100000 \
+                --eval_step 100000 \
+                --seed 42 \
+                --model_name google/t5-v1_1-large \
+                --train_file_name ../T5forNER/ontonotes_fewshot/train.txt \
+                --valid_file_name ../T5forNER/ontonotes_fewshot/valid.txt \
+                --test_file_name ../T5forNER/ontonotes_fewshot/test.txt \
+                --train_sample \
+                --max_length 512 \
+                --adam_epsilon 1e-8 \
+                --warmup_steps 0.01
+              echo "++++++++++++++++++++++++++++++"
+              ps aux | grep T5FTforAllTask.py | awk '{print $2}' | xargs kill -9
+  done
+done
+
+
+
