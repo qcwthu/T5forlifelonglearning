597d80e33b6840a775f7d3322d00773345663a9e
qcw <qcwthu@126.com>
2021-11-16 18:30:04 +0800

    qa

---
+++ T5QAEWC/T5QAEWC.sh
@@ -0,0 +1,46 @@
+#learnrate=(5e-5)
+learnrate=(1e-4)
+allewclambda=(10.0)
+allstartindex=(0 1 2)
+alltask=(0 1 2)
+for onerate in ${learnrate[@]}
+do
+  for onelambda in ${allewclambda[@]}
+  do
+    for onestartindex in ${allstartindex[@]}
+    do
+      for onetask in ${alltask[@]}
+      do
+        echo "------------------------------"
+        python -m torch.distributed.launch --nproc_per_node 1 --master_port 29924 T5QAEWC.py \
+          --cuda 5 \
+          --lr $onerate \
+          --ewc_lambda $onelambda \
+          --startindex $onestartindex \
+          --taskindex $onetask \
+          --weight_decay 0.01 \
+          --max_grad_norm 1.0 \
+          --batch_size_per_gpu 2 \
+          --valid_size_per_gpu 8 \
+          --test_size_per_gpu 8 \
+          --gradient_accumulation_steps 4 \
+          --max_epoch 80 \
+          --num_workers 0 \
+          --save_step 100000 \
+          --eval_step 100000 \
+          --seed 42 \
+          --model T5Summarization \
+          --model_name google/t5-v1_1-large \
+          --train_sample \
+          --max_length 512 \
+          --adam_epsilon 1e-8 \
+          --warmup_steps 0.01
+        echo "++++++++++++++++++++++++++++++"
+        ps aux | grep T5QAEWC.py | awk '{print $2}' | xargs kill -9
+      done
+    done
+  done
+done
+
+
+
---
+++ T5QAEWC/utils.py
@@ -0,0 +1,207 @@
+import torch
+import os
+import numpy as np
+import random
+import csv
+import pickle
+from collections import Counter
+import string
+import re
+def seed_everything(args):
+    random.seed(args.seed)
+    os.environ['PYTHONASSEED'] = str(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+def getfewshot(inpath,outpath,fewshotnum):
+    ###read from inpath
+    intrain = inpath + "/train.txt"
+    invalid = inpath + "/valid.txt"
+    intest = inpath + "/test.txt"
+    alltrainres = []
+    allvalidres = []
+    alltestres = []
+
+    f = open(intrain,'r')
+    while True:
+        oneline = f.readline().strip()
+        if not oneline:
+            break
+        alltrainres.append(oneline)
+    f.close()
+
+    f = open(invalid, 'r')
+    while True:
+        oneline = f.readline().strip()
+        if not oneline:
+            break
+        allvalidres.append(oneline)
+    f.close()
+
+    f = open(intest, 'r')
+    while True:
+        oneline = f.readline().strip()
+        if not oneline:
+            break
+        alltestres.append(oneline)
+    f.close()
+
+    ######select few shot for train valid and test
+    ###outpath
+    fewtrainname = outpath + "/train.txt"
+    fewvalidname = outpath + "/valid.txt"
+    fewtestname = outpath + "/test.txt"
+
+    tousetrainres = random.sample(alltrainres, fewshotnum)
+    tousevalidres = random.sample(allvalidres, fewshotnum)
+    testnum = 500
+    tousetestres = random.sample(alltestres, testnum)
+
+    f = open(fewtrainname,'w')
+    for one in tousetrainres:
+        f.write(one + "\n")
+    f.close()
+
+    f = open(fewvalidname, 'w')
+    for one in tousevalidres:
+        f.write(one + "\n")
+    f.close()
+
+    ####test
+    f = open(fewtestname, 'w')
+    for one in tousetestres:
+        f.write(one + "\n")
+    f.close()
+
+def getpromptembedding(model,tokenizer,promptnumber,taskname):
+    t5_embedding = model.model.get_input_embeddings()
+    promptinitembedding = torch.FloatTensor(promptnumber, t5_embedding.weight.size(1))
+    #print(promptinitembedding)
+    startindex = 0
+    #print(promptinitembedding.shape)
+    #print(t5_embedding.weight.shape)
+    # print(tokenizer.get_vocab())
+    alllabel = ["summarization"]
+    alllabel.append(taskname)
+    print(alllabel)
+    for one in alllabel:
+        encoderes = tokenizer.batch_encode_plus([one], padding=False, truncation=False, return_tensors="pt")
+        touse = encoderes["input_ids"].squeeze()[:-1]
+        # print(touse)
+        # print(touse.shape)
+        embeddingres = t5_embedding(touse).clone().detach()
+        if embeddingres.shape[0] > 1:
+            embeddingres = torch.mean(embeddingres, 0, keepdim=True)
+        promptinitembedding[startindex] = embeddingres
+        startindex += 1
+        # print(embeddingres.shape)
+    #print(promptinitembedding)
+    # alltokens = {}
+    fr = open('allnumber.pickle', 'rb')
+    alltokens = pickle.load(fr)
+    #print(len(alltokens))
+    # print(alltokens)
+    sortedalltoken = sorted(alltokens.items(), key=lambda item: item[1], reverse=True)
+    # print(sortedalltoken)
+    top5000 = []
+    for one in sortedalltoken:
+        if one[0] == 2:
+            continue
+        else:
+            if len(top5000) < 5000:
+                top5000.append(one)
+            else:
+                break
+    #print(len(top5000))
+    vocab = tokenizer.get_vocab()
+    # print(vocab)
+    # for one in top5000:
+    #    print(one[0],"\t",one[1],"\t",tokenizer.convert_ids_to_tokens(one[0]))
+    randomtokennum = promptnumber - len(alllabel)
+    touse = random.sample(top5000, randomtokennum)
+    print(touse)
+    for one in touse:
+        promptinitembedding[startindex] = t5_embedding.weight[one[0]].clone().detach()
+        startindex += 1
+    # print(startindex)
+    # print(promptinitembedding)
+    # print(t5_embedding.weight[2040])
+    return promptinitembedding
+
+def get_grad_params(model):
+    grad_params = []
+    for name,param in model.named_parameters():
+        #ifuseewc = True
+        ifuseewc = False
+        if "model.decoder.block.21" in name or "model.decoder.block.22" in name or "model.decoder.block.23" in name:
+        #if "model.decoder.block.22" in name or "model.decoder.block.23" in name:
+        #if "model.decoder.block.23" in name:
+           ifuseewc = True
+        if ifuseewc and param.requires_grad:
+            grad_params.append(param)
+    return grad_params
+
+def copy_param_data(params):
+    copy_params = []
+    for param in params:
+        copy_params.append(param.data.clone())
+    return copy_params
+
+def param_loss(model, means, fishers, p_lambda, args):
+    grad_params = get_grad_params(model)
+    loss = torch.tensor(0.0).to(args.device)
+    for i, param in enumerate(grad_params):
+        loss += (p_lambda*fishers[i]*(param-means[i])**2).sum()
+    return loss
+
+def gen_fisher(model, args, pre_dataloader):
+    fisher = None
+    model.eval()
+    for step, batch in enumerate(pre_dataloader):
+        model.zero_grad()
+        inputs = {"input_ids": batch[0].to(args.device), "attention_mask": batch[1].to(args.device),
+                  "target_ids": batch[2].to(args.device), "target_mask": batch[3].to(args.device)}
+        loss = model(inputs)
+        finalloss = loss
+        finalloss.backward()
+        grad_params = get_grad_params(model)
+        if fisher is None:
+            fisher = [param.grad**2/len(pre_dataloader)
+                         for param in grad_params]
+        else:
+            fisher = [fisher[i]+param.grad**2/len(pre_dataloader)
+                         for i,param in enumerate(grad_params)]
+    model.train()
+    return fisher
+
+
+
+def normalize_answer(s):
+    def remove_articles(text):
+        return re.sub(r'\b(a|an|the)\b', ' ', text)
+    def white_space_fix(text):
+        return ' '.join(text.split())
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+    def lower(text):
+        return text.lower()
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+def qa_f1_score(prediction, ground_truth):
+    prediction_tokens = normalize_answer(prediction).split()
+    ground_truth_tokens = normalize_answer(ground_truth).split()
+    # prediction_tokens = prediction.split()
+    # ground_truth_tokens = ground_truth.split()
+    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+    num_same = sum(common.values())
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(prediction_tokens)
+    recall = 1.0 * num_same / len(ground_truth_tokens)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
